d4ca4520110e06248604e54c42df91ca702e16d6
Brayan M. Beltre <brayanmbeltre@gmail.com>
2023-12-11 02:24:16 -0400

    Add .env file and update createRoomId function

--- src/lib/createRoomId.ts
+++ src/lib/createRoomId.ts
@@ -1,14 +1,22 @@
-export function createRoomId () {
-  let id = new URLSearchParams(window.location.search).get('room')
+import { answers } from "../words";
+export function createRoomId() {
+  let id = new URLSearchParams(window.location.search).get("room");
   if (!id) {
-    id = randomId()
-    window.location.search = `?room=${id}`
+    id = randomId();
+    window.location.search = `?room=${id}`;
   }
-  return id
+
+  return id;
 }
 
-function randomId () {
-  return 'xxxxxxxxxxxxxxxxxxxxx'.replace(/[x]/g, () => {
-    return (Math.random() * 16 | 0).toString(16)
-  })
+function randomId() {
+  const answer = answers[Math.floor(Math.random() * answers.length)];
+  const b64 = btoa(answer);
+  return b64;
+}
+
+export function getAnswer() {
+  const answer = new URLSearchParams(window.location.search).get("room")!;
+
+  return atob(answer);
 }
